3b3dc7d33725552c6d973146bc3ff8f4443418d2
sagar dhone <sdhone98@gmail.com>
2025-06-19 18:30:06 +0530

    exam app added

---
+++ exam/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.db import models
+from quiz.models import QuizSet, Question
+from resources import QuestionType
+
+
+class QuizAttempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    quiz_set = models.ForeignKey(QuizSet, on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    is_submitted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'quiz_attempt'
+        managed = True
+        unique_together = ('user', 'quiz_set')
+
+
+class UserAnswers(models.Model):
+    attempt = models.ForeignKey(QuizAttempt, on_delete=models.CASCADE, editable=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, editable=False)
+    submitted_ans = models.CharField(
+        max_length=5,
+        choices=QuestionType.choices()
+    )
+
+    class Meta:
+        db_table = "user_answer"
+        managed = True
+        unique_together = ('attempt', 'question')
+
